14168e1060a9ecdde09b8e573299e852edc3757b
h.toriyama <toriyamahiroto1128@gmail.com>
2022-06-08 15:57:05 +0900

    pythonでjsonファイル出力

---
+++ connpass_api.py
@@ -0,0 +1,38 @@
+import requests
+import sys
+import datetime
+from datetime import date
+import json
+
+def get_connpass_json(ymd: date, count: int = 100, order: int = 2):
+    url = "https://connpass.com/api/v1/event/"
+    url += "?ymd="+ymd.isoformat().replace("-", "")+"&count="+str(count)+"&order="+str(order)
+    print(url)
+
+    response = requests.get(url)
+    json_data = response.json()
+
+    return json_data
+
+
+def main():
+    ymd = date.today()
+    if len(sys.argv) > 1 and is_num(sys.argv[1]):
+        ymd += datetime.timedelta(days=float(sys.argv[1]))
+
+    json_data = get_connpass_json(ymd)
+    with open('today.json', 'w', encoding="UTF-8") as f:
+        json.dump(json_data, f, ensure_ascii=False)    
+
+
+def is_num(s) -> bool:
+    try:
+        float(s)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    main()
