92a036de2d1a2861fbc7c554492868599ce44e9d
h.toriyama <toriyamahiroto1128@gmail.com>
2022-06-20 14:27:35 +0900

    1週間分のイベントを取得

--- connpass_api.py
+++ connpass_api.py
@@ -2,6 +2,7 @@ import requests
 import sys
 from datetime import date, datetime, timezone, timedelta
 import json
+import os
 
 def get_connpass_json(ymd: str, count: int = 100, order: int = 2):
     url = "https://connpass.com/api/v1/event/"
@@ -10,30 +11,27 @@ def get_connpass_json(ymd: str, count: int = 100, order: int = 2):
 
     response = requests.get(url)
     json_data = response.json()
-    print(type(json_data))
 
     return json_data
 
 
 def main():
     JST = timezone(timedelta(hours=+9))
-    ymd = datetime.now(JST)
-    if len(sys.argv) > 1 and is_num(sys.argv[1]):
-        ymd += timedelta(days=float(sys.argv[1]))
-    ymd_str = ymd.isoformat().split("T")[0].replace("-", "")
+    ymd_today = datetime.now(JST)
 
-    json_data = get_connpass_json(ymd_str)
-    with open('build/today.json', 'w', encoding="UTF-8") as f:
-        json.dump(json_data, f, ensure_ascii=False)    
+    dir_build = 'build/v1/'
+    os.makedirs(dir_build, exist_ok=True)
 
-
-def is_num(s) -> bool:
-    try:
-        float(s)
-    except ValueError:
-        return False
-    else:
-        return True
+    dir_events = dir_build + 'events/'    
+    os.makedirs(dir_events, exist_ok=True)
+    for i in range(7):
+        dir_day = dir_events + f'{i}/'
+        os.makedirs(dir_day, exist_ok=True)
+        ymd = ymd_today + timedelta(days=float(i))
+        ymd_str = ymd.isoformat().split("T")[0].replace("-", "")
+        json_data = get_connpass_json(ymd_str)
+        with open(f'{dir_day}/index.html', 'w', encoding="UTF-8") as f:
+            json.dump(json_data, f, ensure_ascii=False)    
 
 
 if __name__ == "__main__":
